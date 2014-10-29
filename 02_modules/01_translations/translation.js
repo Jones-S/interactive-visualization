c78a693521d303dafbbf3f19042702cfe9cc2cd0
Jones <jonas.scheiwiller@zhdk.ch>
2014-10-29 15:23:36 +0100

    draw simple circle

--- 02_modules/01_translations/translation.js
+++ 02_modules/01_translations/translation.js
@@ -1,1 +1,16 @@
 // code to translate and zoom on double click event
+
+$(document).ready(function() {
+
+    var svgContainer = d3.select("body").append("svg")
+        .attr("width", 200)
+        .attr("height", 200);
+
+    //Draw the Circle
+    var circle = svgContainer.append("circle")
+        .attr("cx", 30)
+        .attr("cy", 30)
+        .attr("r", 20);
+
+
+});
