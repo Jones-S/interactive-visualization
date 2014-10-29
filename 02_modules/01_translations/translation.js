6adbb40dd9b7fb608023db74abc952ee7637d64c
Jones <jonas.scheiwiller@zhdk.ch>
2014-10-29 15:39:09 +0100

    add double clickable circle

--- 02_modules/01_translations/translation.js
+++ 02_modules/01_translations/translation.js
@@ -2,15 +2,25 @@
 
 $(document).ready(function() {
 
+
     var svgContainer = d3.select("body").append("svg")
         .attr("width", 200)
         .attr("height", 200);
 
+    var circleGroup = svgContainer.append("g");
+
     //Draw the Circle
-    var circle = svgContainer.append("circle")
-        .attr("cx", 30)
-        .attr("cy", 30)
+    var half_height = $("svg").height() / 2;
+    var half_width = $("svg").width() / 2;
+    var circle = circleGroup.append("circle")
+        .attr("cx", half_width)
+        .attr("cy", half_height)
         .attr("r", 20);
+
+    var doubleclick = $("svg circle");
+    doubleclick.dblclick(function() {
+        alert( "Double Clicked" );
+    });
 
 
 });
