36745b3a40880122b4675b6d1df53c8c72753cbe
norisl <noragailer@norsils.local>
2014-10-31 15:41:01 +0100

    object-key für gemeinden

---
+++ 02_modules/04_csvjson/csvjson_total_moveInto.js
@@ -0,0 +1,51 @@
+jQuery(function() {
+	
+	console.log('jQuery start');
+	var $ausgabe;
+
+	$ausgabe = $('#ausgabe');
+	ausgeben('start.....');
+
+	function ausgeben(in_text){
+		$ausgabe.text(in_text);
+	}
+
+
+$.get('gem_def.csv', function( in_data ) {
+	console.log( "-- Daten sind da - length:" + in_data.length );
+    ausgeben( "Daten gelesen.." );
+
+	var line = in_data.split('\n',300);
+	console.log( "Wir haben " + line.length + " Zeilen gefunden" );
+
+	var propertyNames = line[0].split(',');
+	var datacontainer =[];
+
+	for (var index = 1; index < line.length; index += 1) {
+		var single_line = line[index];
+		var partsArray = single_line.split(',');
+		var object = {};
+
+		for (var index2  = 0; index2  < partsArray.length; index2 += 1) {
+			var propertyName = propertyNames[index2];
+			var value = partsArray[index2];
+			object[propertyName] = value
+		}; 
+		datacontainer.push(object);
+	};
+	console.log('pushed'+ datacontainer.length+'object in container');
+
+	console.log(datacontainer);
+
+	var moveInto = {};
+	for (var i = 0; i < datacontainer.length; i++) {
+		var BFSnum = datacontainer[i]['BFS']; // oder datacontainer[i].BFS
+		console.log(BFSnum);
+		var gemeinden = datacontainer[i]['Gemeinden']; // oder datacontainer[i].Gemeinde
+		// console.log(gemeinden.length);
+		moveInto[BFSnum] = gemeinden;
+
+	};
+	console.log(moveInto);
+	});
+});
