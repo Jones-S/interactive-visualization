7853d7c6134ca58bf54f7b561dd19b7d7d7031da
Jones <jonas.scheiwiller@zhdk.ch>
2014-11-02 02:53:23 +0100

    load json locally (async!!!) and add circles with depending radius. & update readme

--- 02_modules/05_datavis/datavis.js
+++ 02_modules/05_datavis/datavis.js
@@ -1,14 +1,38 @@
 $(document).ready(function() {
 
-    //first load json data into array
+//first load json data into array
+//because json is loaded asynchronously, all functions concerning data are written inside
+var data;
 
-    var data; // a global
+d3.json("data.json", function(error, json) {
+    if (error) return console.warn(error);
+    data = json;
 
-    d3.json("data.json", function(error, json) {
-        if (error) return console.warn(error);
-        data = json;
-        console.log(data);
-    });
+    var width = $(window).width();
+    var height = $(window).height();
+    //draw svg
+    var svgContainer = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+    //add group to contain all circles
+    var svgGroup = svgContainer.append("g");
+
+    //add circles
+    var gemCircles = svgGroup.selectAll("circle")
+        .data(data["gemeinden"])
+        .enter()
+        .append("circle");
+
+    var gemCircAttr = gemCircles
+        .attr("cx", 30).attr("cy", 30)
+        .attr("r", function(d, i) {
+            return d["trafficCosts"]/100
+        });
+});
+
+
+
+
 
 
 });
--- README.md
+++ README.md
@@ -16,4 +16,7 @@ http://circos.ca
 introduction to enter() and reading json data
 http://pothibo.com/2013/09/d3-js-how-to-handle-dynamic-json-data/
 
+loading data with higher functions – examples
+http://stackoverflow.com/questions/20506820/d3-a-sub-array-of-objects
+
 ```
