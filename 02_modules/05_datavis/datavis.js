05ddbacd9b7a421cde6e68d38a884e0d14e8f53d
Jones <jonas.scheiwiller@zhdk.ch>
2014-11-02 15:29:50 +0100

    start to fill circleInfo Array with details about center position of circle etc

--- 02_modules/05_datavis/datavis.js
+++ 02_modules/05_datavis/datavis.js
@@ -1,33 +1,102 @@
 $(document).ready(function() {
 
+//first load json data into array
+//because json is loaded asynchronously, all functions concerning data are written inside
+var data;
+
+//mapping function
+function map_range(value, low1, high1, low2, high2) {
+    return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
+}
+
+
+
+d3.json("data.json", function(error, json) {
+    if (error) return console.warn(error);
+    data = json;
+
+    // some necessary global vars
+    var width = $(window).width();
+    var height = $(window).height();
+    var minSize = 2; //min radius of circles independent of population
+    var maxTraffCost = _.max(_.pluck(data["gemeinden"], "trafficCosts")); //use loDash to get max
+
+    // fill circle info array
+    var circleInfo = [];
+    var gemObject = {};
+    _(data["gemeinden"]).forEach(function(gem, i){
+        gemObject = {
+            "name" : data["gemeinden"][i]["gemeinde"],
+            "centerPos" : { "xM" : 0, "yM" : 0 },
+            "paths" : {
+                "diff" : 0,
+                "pathXY" : [{ "px" : 0, "py" : 0}] //x und y pos of path ending on the circle, can have more than one element with x,y if more than 1 line
+            }
+        };
+        circleInfo.push(gemObject);
+    });
 
     //draw svg
     var svgContainer = d3.select("body").append("svg")
-        .attr("width", 600)
-        .attr("height", 600);
+        .attr("width", width)
+        .attr("height", height);
     //add group to contain all circles
     var svgGroup = svgContainer.append("g");
 
+    //calc circle count and angle step
+    var increase = Math.PI * 2 / data["gemeinden"].length;
+    var angle = 0;
+    var rad = 90; //radius of circle with circles on it
+
     //add bg circle
     var bgCircle = svgGroup.append("circle")
-        .attr("cx", 300).attr("cy", 300).attr("r", 120)
-        .attr("fill", "black");
+        .attr("cx", width/2).attr("cy", height/2).attr("r", rad)
+        .style("fill", "none")
+        .style("stroke-dasharray", ("3,3")) // make the stroke dashed
+        .style("stroke", "rgba(255, 255, 255, 0.5)");
 
     //add circles
-    var circles = svgGroup.selectAll(".dataCircle")
-        .data([
-            {cx: "250", cy: "300", r: "12", color: "red"},
-            {cx: "300", cy: "300", r: "12", color: "red"},
-            {cx: "350", cy: "300", r: "12", color: "red"},
-            {cx: "400", cy: "300", r: "12", color: "red"},
-            {cx: "450", cy: "300", r: "12", color: "red"},
-        ])
+    var gemCircles = svgGroup.selectAll(".dataCircle")
+        .data(data["gemeinden"])
         .enter()
-        .append("circle")
-        .attr("cx", function(d) { return d.cx; })
-        .attr("cy", function(d) { return d.cy; })
-        .attr("r", function(d) { return d.r; })
-        .attr("fill", function(d) { return d.color; })
+        .append("circle");
+
+    var gemCircAttr = gemCircles
+        .each(function(d, i){
+            d3.select(this).attr({
+                cx: function(){
+                        var cx = rad * Math.cos(angle) + width/2;
+                        circleInfo[i]["centerPos"]["xM"] = cx;
+                        return cx;
+                    },
+                cy: function(){
+                        var cy = rad * Math.sin(angle) + height/2;
+                        circleInfo[i]["centerPos"]["yM"] = cy;
+                        angle += increase;
+                        return cy;
+                    },
+                r: function(d){
+                    var pop = (d["population"]/11600 < minSize) ? minSize : d["population"]/11600; //if population is producing a too small circle set it to minSize
+                    return pop;
+                    }, //todo: write mapping function and set a max size
+                fill: function(d) {
+                    //use mapping function to map trafficCosts to RGB from 0 - 255
+                    var colorMap = map_range(d["trafficCosts"], 0, maxTraffCost, 255, 120 );
+                    colorMap = Math.floor(colorMap); //and round it to whole numbers to use as rgb
+                    // console.log(colorMap + " - " + d["trafficCosts"]);
+                    return "rgba(" + colorMap +", 0, 0, 0.8)";
+                    }
+            })
+        })
+
+    // _(circleInfo).forEach(function(num){ console.log("hey " + num["centerPos"]["yM"])});
+    
 
 });
 
+
+
+
+
+
+});
