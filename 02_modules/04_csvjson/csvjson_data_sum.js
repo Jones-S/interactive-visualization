367384c4c535a7707dc4934afb293e9f60f463d3
norisl <noragailer@norsils.local>
2014-11-04 18:38:00 +0100

    change names of schluessel

--- 02_modules/04_csvjson/csvjson_data_sum.js
+++ 02_modules/04_csvjson/csvjson_data_sum.js
@@ -1,63 +1,62 @@
 $(document).ready(function() {
 
     var nameKey,
-        moveInData,
-        object = [];
-    resultat = {};
+        moveTo,
+        resultat = {};
+        gemeinden={};
 
     // load data
     $.get("data/gemeinde_nameKey.json", function(data) {
-        console.log("-- Daten sind da --", data);
+        // console.log("-- Daten sind da --", data);
         nameKey = data;
         evalData();
     });
 
     $.get("data/gemeinde_move_to.json", function(data) {
-        console.log("-- Daten sind da --", data);
-        moveInData = data;
+        // console.log("-- Daten sind da --", data);
+        moveTo = data;
         evalData();
     });
 
     $.get('data/gemeinde_leerwoh_2013.json', function(data){
-        console.log("-- Daten sind da --", data);
-        costs = data;
+        // console.log("-- Daten sind da --", data);
+        flats = data;
         evalData();
     });
 
         $.get('data/gemeinde_trafficcost_2012.json', function(data){
-        console.log("-- Daten sind da --", data);
+        // console.log("-- Daten sind da --", data);
         costs = data;
         evalData();
     });
 
     function evalData() {
-        if (nameKey && moveInData && costs ) {
+        if (nameKey && moveTo && flats) {
 
-             function getGemeideName(gemeindeId) {
-                return nameKey[gemeindeId];
-            }
+            //  function getGemeideName(gemeindeId) {
+            //     return nameKey[gemeindeId];
+            // }
 
-            _.forEach(moveInData['gemeinden'], function(gemeindeData) {
+            _.forEach(moveTo['gemeinden'], function(gemeindeData) {
 
-                var reduced = _.reduce(gemeindeData['moveTo'], function(result, key) {
+                var reduced = _.reduce(gemeindeData['moveTo'], function(result, key) { // rechnet Total moveIn Gemeinde aus
                     return parseInt(result, 10) + parseInt(key, 10);
-                }, 0);
+                }, 0);    
 
-                console.log("halloo " + getGemeideName(gemeindeData['gemIdentifier']), reduced);
+                console.log(nameKey[gemeindeData['gemIdentifier']], reduced);
               
             });
 
            var group = [];
-           console.log(moveInData['gemeinden'][0]['moveTo'].length)
-           for (var i = 0; i < moveInData['gemeinden'][0]['moveTo'].length; i++) {
+           // console.log(moveTo['gemeinden'][0]['moveTo'].length);
+           for (var i = 0; i < moveTo['gemeinden'][0]['moveTo'].length; i++) {
                group[i] = 0;
            }
-           console.log(group);
 
-            _.forEach(moveInData['gemeinden'], function (gemeindeData) {
+            _.forEach(moveTo['gemeinden'], function (gemeindeData) { // 
                for (var index = 0; index < gemeindeData['moveTo'].length; index++) {
                     // console.log(group[index],gemeindeData['moveTo'][index])
-                   group[index] += parseInt(gemeindeData['moveTo'][index], 10).push();
+                   group[index] += parseInt(gemeindeData['moveTo'][index], 10);// rechnet Total moveOut Gemeinde
                }
 
             });
@@ -67,18 +66,23 @@ $(document).ready(function() {
             //               }
             console.log(group);
 
+            gemeinden["gemeinden"] = [];
+            _.forEach(nameKey, function(obj, index){
+                var  object = {};
+                object["gemId"]= moveTo['gemeinden'][index]['gemIdentifier'];
+                object["gemName"] = nameKey[index];
+                object["moveOutTot"]=[];
+             
+                console.log(object);
 
-            // // Alle Gemeinden ausgeben
-            // _.forEach(moveInData['gemeinden'], function (gemeindeData) {
-            //     console.log( getGemeinde( gemeindeData['gemIdentifier'] ) );
-            // }); 
+            });
 
         }
     }
 
     //     function getGemMoveOut_total (moveTo) {
     //         var liste = {};
-    //         moveInData.gemeinden()
+    //         moveTo.gemeinden()
 
 
     //         // magic
