8c974cbf9ac0e3512a025b74ed9eae5f1a6d4f78
Jones <jonas.scheiwiller@zhdk.ch>
2014-11-04 22:24:21 +0100

    map lines to circle size and add stroke dash array clean up js file

--- 02_modules/05_datavis/datavis.js
+++ 02_modules/05_datavis/datavis.js
@@ -29,7 +29,7 @@ d3.json("data.json", function(error, json) {
     var height = $(window).height();
     var bgCircCenterX = width/2;
     var bgCircCenterY = height/2;
-    var minSize = 2; //min radius of circles independent of population
+    var minSize = 2; //min radius of circles independent of population in px
     var maxTraffCost = _.max(_.pluck(data["gemeinden"], "trafficCosts")); //use loDash to get max
     var lineAngleStep = Math.PI / 100; //in radians
     //calc circle count and angle step
@@ -41,11 +41,13 @@ d3.json("data.json", function(error, json) {
     var circleInfo = [];
 
     var gemObject = {};
-    _(data["gemeinden"]).forEach(function(gem, i){
+    _(data["gemeinden"]).forEach(function(obj, i){
+        var pop = ((obj["population"]/11600 < minSize) ? minSize : obj["population"]/11600)
         gemObject = {
             "name" : data["gemeinden"][i]["gemeinde"],
             "centerPos" : { "xM" : 0, "yM" : 0 },
-            "connections" : {}
+            "connections" : {},
+            "circleRad" : pop,
             //     // how the content will look like
             //     // "0-0" : {   "delta" : 0,
             //     //             "pathEnds" : [{ "x" : 0, "y" : 0}] //x und y pos of path ending on the circle, can have more than one element with x,y if more than 1 line
@@ -55,7 +57,7 @@ d3.json("data.json", function(error, json) {
 
         var object = {};
 
-        _.forEach(gem["nach"], function(obj, j){
+        _.forEach(obj["nach"], function(elem, j){
             var key = i + "-" + j; //e.g. "0-1" > from gemeinde 0 to gemeinde 1
             //now calculate delta between the gemeinden including itself (always 0)
             gemObject["connections"][key] = { "delta": data["gemeinden"][j]["nach"][i] - data["gemeinden"][i]["nach"][j],
@@ -94,8 +96,12 @@ d3.json("data.json", function(error, json) {
                 halfLines = Math.floor(obj["totalLines"]/2); //round floor because if odd -> one pathending will be centered
             };
             //set first points form left and right in dock
-            var firstRight = obj["angle"] - halfLines * lineAngleStep;
-            var firstLeft = obj["angle"] + halfLines * lineAngleStep;
+            var angleShift = (((obj["circleRad"]*2 - (obj["circleRad"]/3)) / rad)/ totalLines); // still in px ... (obj["circleRad"]*2 ~ b = alpha * rad
+            // var angleShift = 0.01;
+            obj["angleShift"] = angleShift;
+            console.log("angleShift: " + angleShift);
+            var firstRight = obj["angle"] - halfLines * angleShift;
+            var firstLeft = obj["angle"] + halfLines * angleShift;
             obj["firstPoints"] = { "left" : firstLeft, "right" : firstRight }; //save them to the array
             if(obj["totalLines"] > 0){
         };
@@ -121,6 +127,7 @@ d3.json("data.json", function(error, json) {
     //move x,y of pathends
     _.forEach(circleInfo, function(obj, i){ //e.g. {"connection" : ..., "name" ...} etc
         var totalLines = obj["totalLines"];
+        var angleShift = obj["angleShift"];
         var curIndex = i;
         var startLeft = obj["firstPoints"]["left"];
         var startRight = obj["firstPoints"]["right"];
@@ -136,16 +143,14 @@ d3.json("data.json", function(error, json) {
         var plusStep = curIndex;
 
         var increment = (curIndex >= (cncts - curIndex)) ? curIndex : (cncts - curIndex); //set increment var for for loop
-        console.log("increment : " + increment + " minusStep : " + minusStep + " plusStep : " + plusStep + " cncts : " + cncts);
+        // console.log("increment : " + increment + " minusStep : " + minusStep + " plusStep : " + plusStep + " cncts : " + cncts);
 
         for (var j = increment; j >= 0; j--) {
             // console.log("My J: " + j);
             if(minusStep >= 0){
-                console.log("from right " + i + " - " + minusStep);
                 curKey = i + "-" + minusStep; // "3-2"
-                // console.log(obj["connections"][curKey]);
                 _.forEach(obj["connections"][curKey]["pathEnds"], function(elem){
-                    curAttach = startRight + (indexFromRight * lineAngleStep); //calc current Attachment point for line from left
+                    curAttach = startRight + (indexFromRight * angleShift); //calc current Attachment point for line from left
                     x = rad * Math.cos(curAttach) + bgCircCenterX; //calc x depending on current attachment point
                     y = rad * Math.sin(curAttach) + bgCircCenterY; //calc x depending on current attachment point
                     indexFromRight ++; //increase counter from left
@@ -156,52 +161,18 @@ d3.json("data.json", function(error, json) {
             }
 
             if (plusStep <= cncts ) {
-                // console.log("i: " + i);
                 curKey = i + "-" + plusStep; // "3-7"
-                console.log(curKey);
                 _.forEach(obj["connections"][curKey]["pathEnds"], function(elem){
-                    curAttach = startLeft - (indexFromLeft * lineAngleStep); //calc current Attachment point for line from left
+                    curAttach = startLeft - (indexFromLeft * angleShift); //calc current Attachment point for line from left
                     x = rad * Math.cos(curAttach) + bgCircCenterX; //calc x depending on current attachment point
                     y = rad * Math.sin(curAttach) + bgCircCenterY; //calc x depending on current attachment point
                     indexFromLeft ++; //increase counter from left
                     elem["x"] = x;
                     elem["y"] = y;
-                    console.log(elem);
                 });
                 plusStep ++;
             }
         };
-
-
-
-        // //calc x,y for every connection
-        // _.forEach(obj["connections"], function(elem, j){ //e.g. "2-3"
-        //     var linesOfConnection = Math.abs(Math.round(elem["delta"]/20)); //e.g. 7
-        //     var moveFromTo = j.split("-"); // split "0-2" = j into {0, 2} 0= from 2 = to
-        //     var from = moveFromTo[0]; //2
-        //     var to = moveFromTo[1]; //3
-
-        //     if (from != to && from < to) { //if 1-1 dont do nothing, if 0-1, 0-2 etc start add pathends from left side of dock
-        //         for (var i = linesOfConnection - 1; i >= 0; i--) { //for every line calc new x,y from left
-        //             curAttach = startLeft - (indexFromLeft * lineAngleStep); //calc current Attachment point for line from left
-        //             x = rad * Math.cos(curAttach) + bgCircCenterX; //calc x depending on current attachment point
-        //             y = rad * Math.sin(curAttach) + bgCircCenterY; //calc x depending on current attachment point
-        //             // console.log("X: " + x + "   Y: " + y + "    &k: " + k);
-        //             indexFromLeft ++; //increase counter from left
-        //             elem["pathEnds"][i] = {"x" : x, "y": y};
-        //         };
-
-        //     } else if ( from != to && from > to){ //if 1-1 don't do nothing, if 3-0, 3-1 etc start adding pathend coordinates from the right side
-        //         for (var i = linesOfConnection - 1; i >= 0; i--) {
-        //             curAttach = startRight + (indexFromRight * lineAngleStep); //calc current Attachment point for line from left
-        //             x = rad * Math.cos(curAttach) + bgCircCenterX; //calc x depending on current attachment point
-        //             y = rad * Math.sin(curAttach) + bgCircCenterY; //calc x depending on current attachment point
-        //             indexFromRight ++; //increase counter from left
-        //             elem["pathEnds"][i] = {"x" : x, "y": y};
-        //             // k ++;
-        //         };
-        //     };
-        // });
 
     });
 
@@ -238,8 +209,7 @@ d3.json("data.json", function(error, json) {
                         return circleInfo[i]["centerPos"]["yM"];
                     },
                 r: function(d){
-                    var pop = (d["population"]/11600 < minSize) ? minSize : d["population"]/11600; //if population is producing a too small circle set it to minSize
-                    return pop;
+                        return circleInfo[i]["circleRad"];
                     }, //todo: write mapping function and set a max size
                 fill: function(d) {
                     //use mapping function to map trafficCosts to RGB from 0 - 255
@@ -251,7 +221,6 @@ d3.json("data.json", function(error, json) {
             })
         })
 
-    //draw first line
    var line = d3.svg.line()
         .x(function(d) {
             return d.x;
@@ -261,79 +230,18 @@ d3.json("data.json", function(error, json) {
         })
         .interpolate("basis");
 
-    var path = [];
-    path[0] = circleInfo[0]["connections"]["0-1"]["pathEnds"][0];
-    path[1] = { x: bgCircCenterX, y: bgCircCenterY };
-    path[2] = circleInfo[1]["connections"]["1-0"]["pathEnds"][0];
-
-
-    svgGroup.append("path")
-        .data(path)
-        .attr("class", "line")
-        .attr("d", line(path))
-        .attr("fill", "none")
-        .attr("stroke-width", 0.6)
-        .attr("stroke", "#15897a");
-
-    var path = [];
-    path[0] = circleInfo[0]["connections"]["0-1"]["pathEnds"][1];
-    path[1] = { x: bgCircCenterX, y: bgCircCenterY };
-    path[2] = circleInfo[1]["connections"]["1-0"]["pathEnds"][1];
-
-
-    svgGroup.append("path")
-        .data(path)
-        .attr("class", "line")
-        .attr("d", line(path))
-        .attr("fill", "none")
-        .attr("stroke-width", 0.6)
-        .attr("stroke", "#15897a");
-
-    var path = [];
-    path[0] = circleInfo[0]["connections"]["0-1"]["pathEnds"][2];
-    path[1] = { x: bgCircCenterX, y: bgCircCenterY };
-    path[2] = circleInfo[1]["connections"]["1-0"]["pathEnds"][2];
-
-
-    svgGroup.append("path")
-        .data(path)
-        .attr("class", "line")
-        .attr("d", line(path))
-        .attr("fill", "none")
-        .attr("stroke-width", 0.6)
-        .attr("stroke", "#c23400");
-
-        var path = [];
-    path[0] = circleInfo[0]["connections"]["0-1"]["pathEnds"][3];
-    path[1] = { x: bgCircCenterX, y: bgCircCenterY };
-    path[2] = circleInfo[1]["connections"]["1-0"]["pathEnds"][3];
-
-
-    svgGroup.append("path")
-        .data(path)
-        .attr("class", "line")
-        .attr("d", line(path))
-        .attr("fill", "none")
-        .attr("stroke-width", 0.6)
-        .attr("stroke", "#edd400");
-
-
     // draw all lines
     _.forEach(circleInfo, function(obj){
         _.forEach(obj["connections"], function(elem, i){ //e.g. elem = 0-1
             var moveFromTo = i.split("-"); // split "0-2" = i into {0, 2} 0= from 2 = to
-            // console.log(i);
             var from = moveFromTo[0];
             var to = moveFromTo[1];
             var reverseKey = to + "-" + from; //make "2-3" from i = "3-2"
 
             //draw line
             _.forEach(elem["pathEnds"], function(item, j){ //all path elements  -> array with 0-7 e.g.
-                // console.log("ITEM_ :" + item["x"]);
-                // console.log(reverseKey);
                 var x = circleInfo[to]["connections"][reverseKey]["pathEnds"][j]["x"];
                 var y = circleInfo[to]["connections"][reverseKey]["pathEnds"][j]["y"];
-                // console.log("x: " + x + "y: " +y + "OF element " + to + "-" + from + " and pathend " + j);
                 var path = [];
                     path[0] = {"x": item["x"] , "y": item["y"]};
                     path[1] = { x: bgCircCenterX, y: bgCircCenterY }; //center of circle as control point
@@ -344,9 +252,11 @@ d3.json("data.json", function(error, json) {
                     .data(path)
                     .attr("class", "line")
                     .attr("d", line(path))
-                    .attr("fill", "none")
-                    .attr("stroke-width", 0.6)
-                    .attr("stroke", "#edd400");
+                    .style("fill", "none")
+                    .style("stroke-width", 0.6)
+                    .style("stroke", "rgba(254, 255, 223, 0.82)")
+                    .style("stroke-dasharray", "2, 2")
+
             });
         });
     });
@@ -367,7 +277,6 @@ d3.json("data.json", function(error, json) {
         allStartPoints.push(object);
     });
 
-    console.log(allStartPoints);
 
     var startPoints = svgGroup.selectAll(".startCircle")
         .data(allStartPoints)
@@ -386,7 +295,7 @@ d3.json("data.json", function(error, json) {
             })
         })
         .attr("r", "3")
-        .style("fill", "#00ed67")
+        .style("fill", "rgba(0, 237, 103, .4)")
         .attr("class", "startCircle");
 
 
