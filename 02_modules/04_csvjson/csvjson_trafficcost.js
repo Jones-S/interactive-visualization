2cf01302c17744480eaec6e9bbcf94881e4b6866
norisl <noragailer@norsils.local>
2014-11-04 17:42:51 +0100

    csv json modify nettocost & leerwohn

--- 02_modules/04_csvjson/csvjson_trafficcost.js
+++ 02_modules/04_csvjson/csvjson_trafficcost.js
@@ -1,31 +1,25 @@
 jQuery(function() {
 	
 	console.log('jQuery start');
-	var $ausgabe;
-
-	$ausgabe = $('#ausgabe');
-	ausgeben('start.....');
-
-	function ausgeben(in_text){
-		$ausgabe.text(in_text);
-	}
 
 
-$.get('.../gem_def.csv', function( in_data ) {
+	
+
+
+$.get('data/data_traffic_cost_2012.csv', function( in_data ) {
 	console.log( "-- Daten sind da - length:" + in_data.length );
-    ausgeben( "Daten gelesen.." );
 
 	var line = in_data.split('\n',300);
 	console.log( "Wir haben " + line.length + " Zeilen gefunden" );
 
-	var propertyNames = line[0].split(',');
-	var datacontainer =[
+	var propertyNames = line[0].split(';');
+	var datacontainer =[];
 
 	for (var index = 1; index < line.length; index += 1) {
 		line[index]
 
 		var single_line = line[index];
-		var partsArray = single_line.split(',');
+		var partsArray = single_line.split(';');
 		var object = {};
 
 		for (var index2  = 0; index2  < partsArray.length; index2 += 1) {
@@ -39,15 +33,19 @@ $.get('.../gem_def.csv', function( in_data ) {
 
 	console.log(datacontainer);
 
-	var moveInto = {};
-	for (var i = 0; i < datacontainer.length; i++) {
-		var BFSnum = datacontainer[i]['BFS']; // oder datacontainer[i].BFS
-		var gemeinden = datacontainer[i]['Gemeinden']; // oder datacontainer[i].Gemeinde
-		// console.log(gemeinden);
-		moveInto[BFSnum] = gemeinden;
-
+	var nettoCost = {};
+	for (var index = 0; index < datacontainer.length; index++) {
+		var BFS_num = datacontainer[index]['BFS_NR'];
+		var moneyPerPers = datacontainer[index]['INDIKATOR_VALUE'];
+		console.log(datacontainer[index]['INDIKATOR_VALUE']);
+		nettoCost[BFS_num]= moneyPerPers;
 	};
-	console.log(moveInto);
-	window.moveInto = moveInto;
+	console.log(nettoCost);
+	window.nettoCost = nettoCost;
+
 	});
-});
+	
+
+
+
+});// jQuery schliessen
--- 02_modules/04_csvjson/json_modify/csvjson_data_sum.js
+++ 02_modules/04_csvjson/json_modify/csvjson_data_sum.js
@@ -1,43 +1,31 @@
 $(document).ready(function() {
 
-    var nameKey,
-        moveInData,
+    var schluessel,
+        daten,
         object = [];
     resultat = {};
 
     // load data
-    $.get("data/gemeinde_nameKey.json", function(data) {
+    $.get("data/gemeinde_schluessel.json", function(data) {
         console.log("-- Daten sind da --", data);
-        nameKey = data;
+        schluessel = data;
         evalData();
     });
 
-    $.get("data/gemeinde_move_to.json", function(data) {
+    $.get("data/gemeinden_move_to.json", function(data) {
         console.log("-- Daten sind da --", data);
-        moveInData = data;
-        evalData();
-    });
-
-    $.get('data/gemeinde_leerwoh_2013.json', function(data){
-        console.log("-- Daten sind da --", data);
-        costs = data;
-        evalData();
-    });
-
-        $.get('data/gemeinde_trafficcost_2012.json', function(data){
-        console.log("-- Daten sind da --", data);
-        costs = data;
+        daten = data;
         evalData();
     });
 
     function evalData() {
-        if (nameKey && moveInData && costs ) {
+        if (schluessel && daten) {
 
              function getGemeideName(gemeindeId) {
-                return nameKey[gemeindeId];
+                return schluessel[gemeindeId];
             }
 
-            _.forEach(moveInData['gemeinden'], function(gemeindeData) {
+            _.forEach(daten['gemeinden'], function(gemeindeData) {
 
                 var reduced = _.reduce(gemeindeData['moveTo'], function(result, key) {
                     return parseInt(result, 10) + parseInt(key, 10);
@@ -48,28 +36,23 @@ $(document).ready(function() {
             });
 
            var group = [];
-           console.log(moveInData['gemeinden'][0]['moveTo'].length)
-           for (var i = 0; i < moveInData['gemeinden'][0]['moveTo'].length; i++) {
+           console.log(daten['gemeinden'][0]['moveTo'].length)
+           for (var i = 0; i < daten['gemeinden'][0]['moveTo'].length; i++) {
                group[i] = 0;
            }
            console.log(group);
 
-            _.forEach(moveInData['gemeinden'], function (gemeindeData) {
+            _.forEach(daten['gemeinden'], function (gemeindeData) {
                for (var index = 0; index < gemeindeData['moveTo'].length; index++) {
                     // console.log(group[index],gemeindeData['moveTo'][index])
-                   group[index] += parseInt(gemeindeData['moveTo'][index], 10).push();
+                   group[index] += parseInt(gemeindeData['moveTo'][index], 10);
                }
-
             });
-
-            // resultat = {  nameKey[gemeindeId];
-
-            //               }
             console.log(group);
 
 
             // // Alle Gemeinden ausgeben
-            // _.forEach(moveInData['gemeinden'], function (gemeindeData) {
+            // _.forEach(daten['gemeinden'], function (gemeindeData) {
             //     console.log( getGemeinde( gemeindeData['gemIdentifier'] ) );
             // }); 
 
@@ -78,7 +61,7 @@ $(document).ready(function() {
 
     //     function getGemMoveOut_total (moveTo) {
     //         var liste = {};
-    //         moveInData.gemeinden()
+    //         daten.gemeinden()
 
 
     //         // magic
